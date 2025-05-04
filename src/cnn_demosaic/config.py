ab313bf282577d311fc915e27624fbe7c0ab1259
Jacob Jaeggli <13710580+jjaeggli@users.noreply.github.com>
2025-05-04 15:07:19 -0700

    Create separate configuration module for processing.

---
+++ src/cnn_demosaic/config.py
@@ -0,0 +1,128 @@
+from cnn_demosaic import output
+from dataclasses import dataclass
+from importlib import resources
+from pathlib import Path
+
+WEIGHTS_MODULE = "cnn_demosaic.weights"
+DEMOSAIC_WEIGHTS = "x-trans.weights.h5"
+EXPOSURE_WEIGHTS = "exposure.weights.h5"
+COLOR_WEIGHTS = "color.weights.h5"
+WHITE_BALANCE_WEIGHTS = "white_balance.weights.h5"
+
+
+@dataclass
+class Config:
+    raw_filename: str
+    output_filename: str | None = None
+    format: str | None = None
+    color_weights: str | None = None
+    demosaic_weights: str | None = None
+    exposure_weights: str | None = None
+    white_balance_weights: str | None = None
+    crop: bool = True
+    jpeg_crop: bool = False
+    post_process: bool = True
+    fake: bool = False
+
+    def __post_init__(self):
+        self.raw_path = Path(self.raw_filename)
+
+    @property
+    def demosaic_weights_path(self):
+        return get_weights_resource_path(self.demosaic_weights, DEMOSAIC_WEIGHTS)
+
+    @property
+    def color_weights_path(self):
+        return get_weights_resource_path(self.color_weights, COLOR_WEIGHTS)
+
+    @property
+    def exposure_weights_path(self):
+        return get_weights_resource_path(self.exposure_weights, EXPOSURE_WEIGHTS)
+
+    @property
+    def white_balance_weights_path(self):
+        return get_weights_resource_path(self.white_balance_weights, WHITE_BALANCE_WEIGHTS)
+
+    @property
+    def output_suffix(self):
+        return ".exr"
+
+    @property
+    def output_path(self) -> Path:
+        # Either use the default or specified output path.
+        output_path = Path(self.raw_path.with_suffix(self.output_suffix))
+        if self.output_filename is not None:
+            output_path = Path(self.output_filename)
+        return output_path
+
+    def validate_config(self):
+        if not self.raw_path.is_file():
+            raise ValueError(f"The input filename {self.raw_path.absolute()} is not a file!")
+        if self.output_path.exists():
+            raise ValueError(f"The output filename {self.output_path.absolute()} already exists!")
+
+        validate_weights_path(self.demosaic_weights_path)
+
+        # Post processing implies that models other than the demosaicing model
+        # will be used.
+        if self.post_process:
+            validate_weights_path(self.color_weights_path)
+            validate_weights_path(self.exposure_weights_path)
+            validate_weights_path(self.white_balance_weights_path)
+
+    @property
+    def output_handler(self):
+        # TODO(jjaeggli): Output handler should be able to encompas a number of different
+        #   things, such as a bitstream output handler.
+        # Determine the suffix from arguments.
+        _, format_writer = get_format(self.format, self.output_filename)
+
+        def output_handler(output_arr):
+            if format_writer is not None:
+                format_writer(output_arr, self.output_path)
+
+        return output_handler
+
+
+def get_weights_resource_path(resource: str | None, default: str):
+    resource_or_default = default if resource is None else resource
+    resource_path = Path(resources.files(WEIGHTS_MODULE).joinpath(resource_or_default))
+    return resource_path
+
+
+def validate_weights_path(resource_path):
+    if not resource_path.exists():
+        raise ValueError(f"The weights filename {resource_path} is not a file!")
+    if not "".join(resource_path.suffixes) == ".weights.h5":
+        raise ValueError("The weights filename must have the suffix .weights.h5")
+
+
+# TODO(jjaeggli): Extract building the handler to the output module.
+
+
+def get_format_from_str(input_str):
+    format = output.IMG_FORMATS.get(input_str.lower().strip("."))
+    if format is None:
+        raise ValueError(f"Invalid format or unsupported file type specified [{input_str}]")
+    return format
+
+
+def get_format(format_arg, output_arg):
+    use_format = False
+    format_suffix = None
+    format_writer = None
+    if format_arg is not None:
+        use_format = True
+        format_suffix, format_writer = get_format_from_str(format_arg)
+    if output_arg is not None:
+        output_path = Path(output_arg)
+        output_suffix = output_path.suffix.lower()
+        if not use_format:
+            format_suffix, format_writer = get_format_from_str(output_suffix)
+        elif output_suffix != format_suffix:
+            raise ValueError(
+                f"Conflict between specified format and output file suffix: [{output_suffix}]"
+            )
+    if format_suffix is None:
+        return output.IMG_FORMATS[output.EXR_FORMAT]
+    return format_suffix, format_writer
--- src/cnn_demosaic/process_raw.py
+++ src/cnn_demosaic/process_raw.py
@@ -9,6 +9,7 @@ import rawpy
 import tensorflow as tf
 
 from cnn_demosaic import color_model
+from cnn_demosaic import config
 from cnn_demosaic import exposure_model
 from cnn_demosaic.color import Color
 from cnn_demosaic.demosaic import Demosaic
@@ -29,17 +30,10 @@ RAF_SUFFIX = ".raf"
 logger = logging.getLogger()
 
 
-def process_raw(
-    raw_path: pathlib.Path,
-    weights_path: pathlib.Path,
-    exposure_weights_path: pathlib.Path,
-    color_weights_path: pathlib.Path,
-    output_handler,
-    fake=False,
-    crop=False,
-    post_process=True,
-):
+def process_raw(processing_config: config.Config):
     """Performs raw image processing on the specified file."""
+    raw_path = processing_config.raw_path
+
     with rawpy.imread(str(raw_path)) as raw_img:
         raw_img_arr = raw_img.raw_image.astype(np.float32).copy()
         raw_img_sizes = raw_img.sizes
@@ -48,23 +42,28 @@ def process_raw(
 
     loaded_model = None
 
-    is_xtrans = raw_path.suffix.lower() == RAF_SUFFIX
-
+    # TODO(jjaeggli): Update this value to match the training input value range.
     wb_scale = math.fsum(daylight_whitebalance) / math.fsum(camera_whitebalance)
     wb_matrix = np.asarray(camera_whitebalance)[:3] * wb_scale
 
-    if is_xtrans:
-        if fake:
-            loaded_model = model.create_fake_xtrans_model(weights_path)
-        else:
-            loaded_model = model.create_xtrans_model(weights_path)
-    else:
-        loaded_model = model.create_32_64_32_model(weights_path)
+    demosaic_weights_path = processing_config.demosaic_weights_path
 
-    exp_model = exposure_model.create_exposure_model(exposure_weights_path)
+    # TODO(jjaeggli): Actually look at the sensor format to determine which sensor
+    #   format should be used.
+    is_xtrans = raw_path.suffix.lower() == RAF_SUFFIX
+
+    if is_xtrans:
+        if processing_config.fake:
+            loaded_model = model.create_fake_xtrans_model(demosaic_weights_path)
+        else:
+            loaded_model = model.create_xtrans_model(demosaic_weights_path)
+    else:
+        loaded_model = model.create_32_64_32_model(demosaic_weights_path)
+
+    exp_model = exposure_model.create_exposure_model(processing_config.exposure_weights_path)
     exposure = Exposure(exp_model)
 
-    c_model = color_model.build_color_model(color_weights_path)
+    c_model = color_model.build_color_model(processing_config.color_weights_path)
     color = Color(c_model)
 
     per_tile_fn = transform.adj_levels_per_tile_fn
@@ -73,19 +72,17 @@ def process_raw(
     raw_img_arr = transform.normalize_arr(raw_img_arr)
     output_arr = processor.demosaic(raw_img_arr)
 
-    if post_process:
-        logger.info(
-            "Performing post-processing."
-        )
+    if processing_config.post_process:
+        logger.info("Performing post-processing.")
         output_arr = exposure.process(output_arr)
         output_arr = color.process(output_arr, wb_matrix)
 
     output_arr = np.asarray(output_arr, dtype=np.float32)
 
-    if crop:
+    if processing_config.crop:
         output_arr = crop_image(output_arr, raw_img_sizes)
 
-    output_handler(output_arr)
+    processing_config.output_handler(output_arr)
 
 
 def crop_image(img_arr, img_sizes: rawpy.ImageSizes):
@@ -109,34 +106,6 @@ def crop_image(img_arr, img_sizes: rawpy.ImageSizes):
     return img_arr[row_start:row_end, col_start:col_end]
 
 
-def get_format_from_str(input_str):
-    format = output.IMG_FORMATS.get(input_str.lower().strip("."))
-    if format is None:
-        raise ValueError(f"Invalid format or unsupported file type specified [{input_str}]")
-    return format
-
-
-def get_format(format_arg, output_arg):
-    use_format = False
-    format_suffix = None
-    format_writer = None
-    if format_arg is not None:
-        use_format = True
-        format_suffix, format_writer = get_format_from_str(format_arg)
-    if output_arg is not None:
-        output_path = pathlib.Path(output_arg)
-        output_suffix = output_path.suffix.lower()
-        if not use_format:
-            format_suffix, format_writer = get_format_from_str(output_suffix)
-        elif output_suffix != format_suffix:
-            raise ValueError(
-                f"Conflict between specified format and output file suffix: [{output_suffix}]"
-            )
-    if format_suffix is None:
-        return output.IMG_FORMATS[output.EXR_FORMAT]
-    return format_suffix, format_writer
-
-
 def main():
     default_weights_path = resources.files(WEIGHTS_MODULE).joinpath(DEFAULT_WEIGHTS)
 
@@ -157,52 +126,26 @@ def main():
 
     keras.utils.disable_interactive_logging()
 
-    raw_path = pathlib.Path(args.raw_filename)
-    if not raw_path.is_file():
-        raise ValueError(f"The input filename {raw_path} is not a file!")
+    post_process = not args.nopost
 
-    weights_path = pathlib.Path(args.weights)
-    if not weights_path.is_file():
-        raise ValueError(f"The weights filename {weights_path} is not a file!")
-    if not "".join(weights_path.suffixes) == ".weights.h5":
-        raise ValueError("The weights filename must have the suffix .weights.h5")
+    processing_config = config.Config(
+        args.raw_filename,
+        output_filename=args.output,
+        format=args.format,
+        demosaic_weights=args.weights,
+        crop=args.crop,
+        post_process=post_process,
+        fake=args.fake,
+    )
 
-    exposure_weights_path = pathlib.Path(resources.files(WEIGHTS_MODULE).joinpath(EXPOSURE_WEIGHTS))
-    if not exposure_weights_path.is_file():
-        raise ValueError(f"The exposure weights filename {exposure_weights_path} is not a file!")
+    processing_config.validate_config()
 
-    color_weights_path = pathlib.Path(resources.files(WEIGHTS_MODULE).joinpath(COLOR_WEIGHTS))
-    if not color_weights_path.is_file():
-        raise ValueError(f"The color weights filename {color_weights_path} is not a file!")
-
-    # Determine the suffix from arguments.
-    format_suffix, format_writer = get_format(args.format, args.output)
-
-    # Either use the default or specified output path.
-    output_path = pathlib.Path(raw_path.with_suffix(format_suffix))
-    if args.output is not None:
-        output_path = pathlib.Path(args.output)
-    if output_path.exists():
-        raise ValueError(f"The output filename {output_path.absolute()} already exists!")
+    output_path = processing_config.output_path
+    raw_path = processing_config.raw_path
 
     logger.debug(f'Processing: ["{raw_path.absolute()}"] => "{output_path.absolute()}"')
 
-    def output_handler(output_arr):
-        if format_writer is not None:
-            format_writer(output_arr, output_path)
-
-    post_process = not args.nopost
-
-    process_raw(
-        raw_path,
-        weights_path,
-        exposure_weights_path,
-        color_weights_path,
-        output_handler,
-        fake=args.fake,
-        crop=args.crop,
-        post_process=post_process
-    )
+    process_raw(processing_config)
 
 
 if __name__ == "__main__":
